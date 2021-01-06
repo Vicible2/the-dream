27dad90781cd99ffe903035e8177d71d6a34fee9
Manten Devillé <manten.d@hotmail.com>
2021-01-06 14:18:14 +0100

    Edit option values and choices, style html form layout

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<h1>Going abroad?</h1>
+<h2>Find out how much you're actually spending!</h2>
+    <form>
+        <label for="currenyInput">Fill in the amount: €</label>
+        <br>
+        <input type="number" placeholder="499">
+        <label for="EUR">
+        <br>
+        <br>
+            Select desired currency:
+        </label>
+        <br>
+        <select id="currency" name="currency">
+        <option value="none"></option>
+            <option value="NOK">NOK</option>
+            <option value="USDollar">USD</option>
+           
+        </select>
+        <input type="submit" value="Submit">
+
+
+
+    </form>
+</body>
+
+</html>
